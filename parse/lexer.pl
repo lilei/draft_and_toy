7c4864115253cbc9d8ea2358076462267fe1b7ee
lilei <lilei5678@163.com>
2016-11-22 17:11:50 +0800

    reuse consume to WS and NAME

--- parse/lexer.pl
+++ parse/lexer.pl
@@ -1,15 +1,23 @@
 #! /usr/bin/perl
+
+=pod
+list			: '[' elements ']';
+elements	: element (',' element)+;
+element		: NAME | list;
+NAME		: (a .. z | A .. Z)+;
+=cut
+
 use utf8;
 use warnings;
 use strict;
 use 5.010;
 
-my $str = ($ARGV[0] or  "[ab ,  bc   ]");
+my $str = ($ARGV[0] or  "[ab ,  bc,lilei   ]");
 
 my @input = split //,$str;
 my $len = @input;
-my $offset  = 0;
-my $c = $input[0];
+my $offset  = 0;		#当前偏移量
+my $c = $input[0];  #向前看词法单元
 
 my $EOF 	= 1;
 my $NAME 	= 2;
@@ -24,34 +32,35 @@ my @tokenNames = qw#n/a  <EOF> NAME COMMA  LBRACK RBRACK EQUALS#;
 while(&nextToken() != $EOF ){
 }
 
-
 sub nextToken(){
 	if($c =~ /\s/){
 		&WS();
 	}
-	while($c ne "<EOF>"){
-		if($c eq ','){
-			&consume();
-			return &Token($COMMA,",");
-		}elsif($c eq '['){
-			&consume();
-			return &Token($LBRACK,"[");
-		}elsif($c eq ']'){
-			&consume();
-			return &Token($RBRACK,"]");
-		}elsif($c eq '='){
-			&consume();
-			return &Token($EQUALS,"=");
-		}else{
-			if(&isLETTER())	{
-				return &NAME();
-			}else{
-				say "Invalid character $c,$offset";
-				return $EOF;
-			}
-		}			
+	if($c eq "<EOF>"){
+		return &Token($EOF,"<EOF>");
 	}
-	return &Token($EOF,"<EOF>");
+	if($c eq ','){
+		&consume();
+		return &Token($COMMA,",");
+	}
+	if($c eq '['){
+		&consume();
+		return &Token($LBRACK,"[");
+	}
+	if($c eq ']'){
+		&consume();
+		return &Token($RBRACK,"]");
+	}
+	if($c eq '='){
+		&consume();
+		return &Token($EQUALS,"=");
+	}
+	if(&isLETTER())	{
+		return &NAME();
+	}
+
+	say "Invalid character $c,$offset";
+	return $EOF;
 }
 
 sub isLETTER{
@@ -67,25 +76,17 @@ sub Token{
 }
 
 sub NAME{
-	my $text;
-	foreach (@input[$offset .. $#input]){
-		if(/[a-zA-Z]/){
-			$text .= $_;
-			&consume();
-			next;
-		}else{
-			return &Token($NAME,$text);
-		}
+	my $text = $c;
+	while ($c =~ /[a-zA-Z]/) {
+		&consume();
+		$text .= $c
 	}
+	return &Token($NAME,$text)
 }
 
 sub WS{
-	foreach(@input[$offset .. $#input]){
-		if(/\s/){
-			&consume();	
-		}else{
-			return;
-		}
+	while ($c =~ /\s/) {
+		&consume();
 	}
 }
 
@@ -94,18 +95,6 @@ sub consume{
 	if($offset >= $len){
 		$c = "<EOF>";
 	}else{
-		$c = $input[$offset];		
-	}	
- } 
-
-sub match{
-	my $x = $_[0];
-	if($c eq $x){
-		&consume();
+		$c = $input[$offset];
 	}
-	else{
-		say "error,expecting $x,found $c";
-	}
-}
-
-
+ }
