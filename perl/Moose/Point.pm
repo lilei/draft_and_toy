d3ad712481f91d259e1248434322390fc97e9678
lilei <lilei5678@163.com>
2016-11-23 12:39:49 +0800

    a sample of Moose

---
+++ perl/Moose/Point.pm
@@ -0,0 +1,13 @@
+
+package Point;
+use Moose;
+has 'x' => (is => 'rw', isa => 'Int');
+has 'y' => (is => 'rw', isa => 'Int');
+
+sub clear{
+  my $self = shift;
+  $self->x(0);
+  $self->y(0);
+};
+
+1;
---
+++ perl/Moose/test-point.pl
@@ -0,0 +1,11 @@
+#! /usr/bin/perl
+use utf8;
+use warnings;
+use strict;
+
+use Point;
+package main;
+my $p = Point->new(
+  x => 0,
+  y => 0
+);
